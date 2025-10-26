4fc461076f509dfbeaf6184442473a15dda1f870
AP-DOWN- <22fmif.a.pribora@std.npu.edu.ua>
2025-10-26 21:56:14 +0200

    added avg student score and filter for group name

--- main.py
+++ main.py
@@ -14,16 +14,11 @@ for student in all_students:
             avg_score += int(student[score])
     student["Середня оцінка"] = avg_score / 4
 
+#4.1.
 while True:
     target_group = input("Введіть назву ОДНІЄЇ групи для експорту: ")
     if target_group in set(all_students[group_name]['Група'] for group_name in range(len(all_students))):
-
-        print("poka rabotaet")
-
         break
     else:
         print("Такої групи не існує, спрообуйте ще раз.")
 
-request = input("Введіть назву однієї групи для експорту: ")
-while request not in set(all_students[i]['Група'] for i in range(len(all_students))):
-    request = input("Введіть дійсну назву групи для експорту:")
